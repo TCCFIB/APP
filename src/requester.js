6e113114c4ca8bf80b4205ef0562c825c3a316fe
Helder Traci <helderdiin.work@gmail.com>
2019-03-09 08:41:28 -0300

    add new baseUrl

--- src/requester.js
+++ src/requester.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const storage = window.localStorage;
-const baseUrl = storage.getItem('baseUrl') ? storage.getItem('baseUrl') : 'http://localhost/API/public/api';
+const baseUrl = storage.getItem('baseUrl') ? storage.getItem('baseUrl') : 'http://rayssa.simpleuse.com.br/api';
 
 /* eslint-disable max-len */
 export default axios.create({
